5dd4ed39d20caef4b7338a4afe8ec28afc6353e5
Jasmine <jasminewilliams2017@gmail.com>
2020-04-12 18:40:44 -0500

    data validated, documentation added with swagger

--- WalkItAPI/Models/Owner.cs
+++ WalkItAPI/Models/Owner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,12 @@ namespace WalkItAPI.Models
     public class Owner
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(40, MinimumLength = 2)]
         public string Name { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public int NeighborhoodId { get; set; }
         public string Phone { get; set; }
 
--- WalkItAPI/Startup.cs
+++ WalkItAPI/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace WalkItAPI
 {
@@ -25,6 +28,16 @@ namespace WalkItAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WalkItAPI", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +47,12 @@ namespace WalkItAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WalkItAPI V1");
+            });
 
             app.UseRouting();
 
